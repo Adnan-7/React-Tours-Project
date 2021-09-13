3fccf12c7a73c1544c0aa2d5d4157a1d14d95cdc
Adnan Khan <adnan7.zain@gmail.com>
2021-09-13 12:40:40 +0500

    Fetching tours data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
-    <div>
-      <h1>tours project</h1>
-    </div>
+    <main>
+      <Tours />
+    </main>
   );
 }
 
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Tours() {
-  return <div>Tours component</div>;
+  return (
+    <div className='title'>
+      <h2>Tours component</h2>
+    </div>
+  );
 }
 
 export default Tours;
